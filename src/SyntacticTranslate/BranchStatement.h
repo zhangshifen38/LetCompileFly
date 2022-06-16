69fc19430bb96aed54add8432a7f1a02ca775754
AlexHoring <alexhoring@qq.com>
2022-06-16 16:08:23 +0800

    小修改，新建了分支语句判断文件

---
+++ src/SyntacticTranslate/BranchStatement.h
@@ -0,0 +1,14 @@
+//
+// Created by AlexHoring on 2022/6/16.
+//
+
+#ifndef LETCOMPILEFLY_BRANCHSTATEMENT_H
+#define LETCOMPILEFLY_BRANCHSTATEMENT_H
+
+
+class BranchStatement {
+
+};
+
+
+#endif //LETCOMPILEFLY_BRANCHSTATEMENT_H
